7c22e919d50f2e28be8feedd989a07a80a197e67
Sanket Sarkar <sscodes@ol7-19.localdomain>
2023-12-21 14:40:15 +0530

    frontend redux issue resolved

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,13 +1,13 @@
 import { applyMiddleware, compose, createStore } from 'redux';
-import { thunk } from 'redux-thunk';
+import { thunk } from 'redux-thunk'; // Correct import
+
 import rootReducer from '../reducers/index';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(thunk)) // Use thunk middleware correctly
 );
 
 export default store;
