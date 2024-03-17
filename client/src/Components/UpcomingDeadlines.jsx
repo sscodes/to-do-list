0bf9296a3910771b05ab03eac107962829e4f939
Sanket Sarkar <sscodes@ol7-19.localdomain>
2024-03-17 17:28:27 +0530

    upcoming tasks bugfix

--- client/src/Components/UpcomingDeadlines.jsx
+++ client/src/Components/UpcomingDeadlines.jsx
@@ -4,10 +4,12 @@ import { useSelector } from 'react-redux';
 const UpcomingDeadlines = () => {
   const tasks = useSelector((state) => state.tasks.tasks);
 
-  const alertTasks = tasks.filter((task) => {
-    const diffTime = Math.abs(new Date(task.deadline) - new Date());
-    return Math.ceil(diffTime / (1000 * 60 * 60 * 24)) < 7;
-  });
+  const alertTasks = tasks
+    .filter((task) => {
+      const diffTime = Math.abs(new Date(task.deadline) - new Date());
+      return Math.ceil(diffTime / (1000 * 60 * 60 * 24)) < 7;
+    })
+    .filter((task) => !task.done);
 
   const sortedAlertTasks = alertTasks.sort(
     (a, b) => new Date(a.deadline) - new Date(b.deadline)
